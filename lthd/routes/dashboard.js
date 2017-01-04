1d42fb7df4e505b9c44b48de0fd725c9280a3c64
ThanhHieu0195 <https://github.com/ThanhHieu0195/ariline>
2017-01-04 15:16:06 +0700

    profile

--- lthd/routes/dashboard.js
+++ lthd/routes/dashboard.js
@@ -2,4 +2,9 @@ module.exports = function (router, passport) {
     router.get('/', function (req, res) {
         res.render('dashboard');
     });
+
+    router.get('/profile', function (req, res) {
+        res.render('profile');
+    });
+
 }
